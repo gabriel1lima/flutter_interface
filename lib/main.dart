ec24b5a68f5a8d9a8d0709a3fc18d5ed00394dcd
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2020-07-07 09:33:31 -0300

    Configuring main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test_interface/widgets/cardItem.dart';
+import 'package:test_interface/widgets/itemList.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'Helvetica-Regular',
+        primarySwatch: Colors.red,
+      ),
       home: MyHomePage(),
     );
   }
@@ -20,18 +26,19 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topCenter,
+            begin: Alignment.topLeft,
             end: Alignment.bottomCenter,
             colors: [
-              Colors.red,
-              Colors.red[400],
-              Colors.red[200],
+              Color(0xFFD9190F),
+              // Colors.red[400],
+              Color(0xFFF58577),
             ],
           ),
         ),
@@ -40,16 +47,10 @@ class MyHomePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Padding(
-                padding: new EdgeInsets.all(20.0),
-                child: Container(
-                  height: height * 0.3,
-                  // decoration: BoxDecoration(
-                  //   border: Border.all(
-                  //     width: 2.0,
-                  //     color: Colors.green,
-                  //   ),
-                  // ),
+              Container(
+                height: height * 0.3,
+                child: Padding(
+                  padding: new EdgeInsets.only(top: 20, left: 20, right: 20),
                   child: Column(
                     children: <Widget>[
                       Row(
@@ -72,7 +73,7 @@ class MyHomePage extends StatelessWidget {
                                 ),
                               ),
                               Text(
-                                '1',
+                                '2',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 16,
@@ -82,9 +83,7 @@ class MyHomePage extends StatelessWidget {
                           )
                         ],
                       ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
+                      SizedBox(height: 25),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
@@ -92,8 +91,7 @@ class MyHomePage extends StatelessWidget {
                             'Food',
                             style: TextStyle(
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30.0,
+                              fontSize: 30,
                             ),
                           ),
                           Icon(
@@ -103,100 +101,101 @@ class MyHomePage extends StatelessWidget {
                           )
                         ],
                       ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
+                      SizedBox(height: 25),
                       Flexible(
                         child: ListView(
                           scrollDirection: Axis.horizontal,
                           children: <Widget>[
-                            Container(
-                              margin: new EdgeInsets.only(right: 20.0),
-                              child: Column(
-                                children: <Widget>[
-                                  SizedBox(
-                                    height: 70,
-                                    width: 70,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(14),
-                                      ),
-                                      child: Icon(
-                                        Icons.fastfood,
-                                        color: Colors.red,
-                                        size: 28.0,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    'Burger',
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Container(
-                              margin: new EdgeInsets.only(right: 20.0),
-                              child: Column(
-                                children: <Widget>[
-                                  SizedBox(
-                                    height: 70,
-                                    width: 70,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.red[600],
-                                        borderRadius: BorderRadius.circular(14),
-                                      ),
-                                      child: Icon(
-                                        Icons.local_pizza,
-                                        color: Colors.white,
-                                        size: 28.0,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    'Pizzas',
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ],
-                              ),
-                            ),
+                            CardItem('pizza.png', true),
+                            CardItem('burger.png', false),
+                            CardItem('hot-dog.png', false),
+                            CardItem('fries.png', false),
+                            CardItem('ice-cream.png', false),
                           ],
                         ),
                       ),
-                      // Row(
-                      //   children: <Widget>[
-
-                      //   ],
-                      // )
                     ],
                   ),
                 ),
               ),
-              Padding(
-                padding: new EdgeInsets.only(left: 10, right: 10, top: 10),
-                child: Container(
-                  height: (height * 0.70) - 20 - 20 - statusBarHeight - 10,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35),
-                      topRight: Radius.circular(35),
-                    ),
-                    // border: Border.all(
-                    //   width: 2.0,
-                    //   color: Colors.green,
-                    // ),
+              Container(
+                height: (height * 0.70) - statusBarHeight,
+                width: double.infinity,
+                child: Padding(
+                  padding: new EdgeInsets.only(left: 12, right: 12, top: 12),
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(35),
+                            topRight: Radius.circular(35),
+                          ),
+                        ),
+                        child: Padding(
+                          padding:
+                              new EdgeInsets.only(top: 30, left: 23, right: 23),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    'Menu',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontFamily: 'Helvetica-Bold',
+                                      color: Color(0xFF000000),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20),
+                              ItemList(
+                                'Brooklyn',
+                                'https://i.pinimg.com/originals/c9/57/80/c957805c620f8ec9ffe7e365674dc659.png',
+                                '350g',
+                                '8.00',
+                                true,
+                              ),
+                              SizedBox(height: 25),
+                              ItemList(
+                                'New York',
+                                'https://static.expressodelivery.com.br/imagens/produtos/47476/180/Expresso-Delivery_b626c98d1108e969c9ac1a37a087b26d.png',
+                                '250g',
+                                '5.00',
+                                false,
+                              ),
+                              SizedBox(height: 25),
+                              ItemList(
+                                'Dallas',
+                                'https://bupaqe.com/wp-content/uploads/2017/05/cardapio_sanduiche_02.png',
+                                '300g',
+                                '7.00',
+                                false,
+                              ),
+                              SizedBox(height: 25),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        height: 4,
+                        top: 20,
+                        left: (width / 2) - 27,
+                        child: Container(
+                          height: 4,
+                          width: 27,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(3),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
